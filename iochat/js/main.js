8f6e3629193d31739dd6748ad8399ea1de578d20
Pranav Singhal <pranavsinghal96@gmail.com>
2018-10-04 16:15:39 -0700

    added webview sockets

--- iochat/js/main.js
+++ iochat/js/main.js
@@ -1,22 +1,28 @@
 $(function(){
-console.log('document ready');
+  console.log('document ready');
 
 
-$('#show-form-button').click(function(){
+  $('#show-form-button').click(function(){
 
-  $(this).css('display','none');
-  $('#registration-form').removeClass('hidden')
-})
+    $(this).css('display','none');
+    $('#registration-form').removeClass('hidden')
+  })
 
-$('#device-registration-button').click(function(e){
-  e.preventDefault();
-  console.log("here ");
-  //verify from mongo if username is unique
-    //if unique ,
-      //then save to db
+  $('#device-registration-button').click(function(e){
+    e.preventDefault();
+    console.log("here ");
+    var username = $('input[name="username"]').val();
+    //verify from mongo if username is unique
+    //if !unique ,
+    //warn user to choose new username
+    //else save to db, then connect socket with the username
+    socket = io();
+    socket.on('connect',function(){
+      console.log("connect callaed from front end");
+      console.log(username);
+      socket.emit('setNewDevice',{clientId: username});
+    })
 
-    //else warn user to choose new username
-
-})
+  })
 
 })
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 users = [] ;
 connections = []  ;
+nodes =[]
 
 server.listen(process.env.PORT || 3000) ;
 console.log('server running at port 3000');
@@ -11,7 +12,27 @@ app.use('/', express.static('public_static'));
 
 
 //webview for the android app running on iochat
+
+
+//on disconnect, remove that user from the array of nodes by finding him in that array using his clientId
 app.use('/app',express.static('iochat'))
 io.on('connection', function(socket){
   console.log('a user connected');
+  console.log(socket.id);
+  socket.on('setNewDevice',function(data){
+    nodes.push({clientId: data.clientId, socket: socket})
+    console.log(nodes.length);
+  })
+
+  socket.on('disconnect',function(data){
+    let self = this
+    temp = nodes.find(function(obj){
+
+      console.log(self.id);
+      return obj.socket.id === self.id;
+
+    });
+    console.log(temp.clientId);
+
+  });
 });
